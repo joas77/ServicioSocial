a6aebb78cbc8fa4eb739541bc97f62185cb3390c
Julio <solojoas@gmail.com>
2018-08-02 08:38:57 -0500

    see many frames in same window
    Using cvWindow class in order to see many frames in same window

--- src/personCounter.py
+++ src/personCounter.py
@@ -2,39 +2,68 @@
 """
 Autor: Julio Angulo
 """
-import numpy as np
 import cv2
+import cvGui
 
-#cap = cv2.VideoCapture('vtest.avi')
-#
-#fgbg = cv2.createBackgroundSubtractorMOG2()
-#
-#while(1):
-#    ret, frame = cap.read()
-#
-#    fgmask = fgbg.apply(frame)
-#
-#    cv2.imshow('frame',fgmask)
-#    k = cv2.waitKey(30) & 0xff
-#    if k == 27:
-#        break
-#
-#cap.release()
-#cv2.destroyAllWindows()
+MIN_AREA = 150
 
-cap = cv2.VideoCapture(0)
+if __name__=="__main__":
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    WIN_NAMES = ["background",
+               "Morphologycal operation",
+               "image raw",
+               "blurred"]
+    
+    video = 0#"../videos/people_walking_test_video1.mp4"
+    cap = cv2.VideoCapture(video)
+    fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+    
+    # creating window   
+    window = cvGui.Window("Test",
+                          cap.get(cv2.CAP_PROP_FRAME_HEIGHT),
+                          cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+                          2,2)   
+    window.create()
+    
+    key = 0;
+    while(key != ord('q')):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret == False:
+            break #video ended or camera failed
+            
+        blur = cv2.GaussianBlur(frame, (5,5), 10)
+        fgmask = fgbg.apply(blur)
+        
+        window.update(blur, (0,1))
+        window.update(fgmask, (1,1), "binary")
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything done, release the capture
-cap.release()
+        #dilate 
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+        morph_op = cv2.morphologyEx(fgmask, 
+                                    cv2.MORPH_CLOSE, 
+                                    kernel, 
+                                    iterations=5)
+        
+        window.update(morph_op, (1,0), "binary")
+        
+        # finding contours
+        _, contours, hierarchy = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, 
+                                    cv2.CHAIN_APPROX_SIMPLE)
+        
+        #drawing bounding boxes
+        for c in contours:
+            if cv2.contourArea(c) > MIN_AREA:
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+        window.update(frame, (0,0))   
+        window.show()
+        
+        #TODO: add ROi
+        key = cv2.waitKey(1)
+        
+    
+    # When everything done, release the capture
+    cap.release()
+    window.destroy()
